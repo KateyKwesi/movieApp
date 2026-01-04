01209390e5978a87ca31e93be6564f57f30e68e0
kateykwesi <kateykwesi001@gmail.com>
2026-01-04 05:37:00 +0000

    fix: iframe orientation for tv

--- src/components/TV/watchTV.jsx
+++ src/components/TV/watchTV.jsx
@@ -7,7 +7,7 @@ function WatchTV() {
   const [loading, setLoading] = useState(true);
 
   return (
-    <div className="relative w-full h-screen">
+    <div className="fixed episode-scroll inset-0 bg-black bg-opacity-70 backdrop-blur-lg flex justify-center items-center ">
       {loading && (
         <div className="absolute inset-0 flex items-center justify-center text-amber-50 z-10">
           <h1 className="inline-block text-slate-400 text-2xl tracking-widest  font-semibold  m-5 ">
@@ -15,13 +15,16 @@ function WatchTV() {
           </h1>
         </div>
       )}
-      <IframeResizer
-        className="relative z-50"
-        log="collapsed"
-        src={`https://player.vidplus.to/embed/tv/${id}/${season}/${episode}?autoplay=true&autonext=true&nextbutton=true&poster=true&title=true&watchparty=false&chromecast=true&episodelist=true&servericon=true&setting=true&pip=true&primarycolor=6C63FF&secondarycolor=9F9BFF&iconcolor=FFFFFF&font=Roboto&fontcolor=FFFFFF&fontsize=20&opacity=0.5`}
-        style={{ width: "100%", height: "100vh" }}
-        onLoad={() => setLoading(false)}
-      />
+      <div className=" w-full h-[40vh]  ">
+        <iframe
+          className="relative w-full h-full z-50"
+          src={`https://player.vidplus.to/embed/tv/${id}/${season}/${episode}?autoplay=true&autonext=true&nextbutton=true&poster=true&title=true&watchparty=false&chromecast=true&episodelist=true&servericon=true&setting=true&pip=true&primarycolor=6C63FF&secondarycolor=9F9BFF&iconcolor=FFFFFF&font=Roboto&fontcolor=FFFFFF&fontsize=20&opacity=0.5`}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+          allowFullScreen
+          onLoad={() => setLoading(false)}
+        ></iframe>
+      </div>
     </div>
   );
 }
